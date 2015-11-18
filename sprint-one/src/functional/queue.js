e3cce19c8b5bb9f5d940e10281d993b620d5c863
Kristen Haydel <kristen@whatever.com>
2015-11-18 14:12:09 -0600

    Implemented Stack and Queue Functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -5,14 +5,36 @@ var Queue = function(){
   var storage = {};
 
   // Implement the methods below
+// storage = {0:a,1:b,2:c}
+// storage[0] = a, storage[1] = b, storage[2] = c
+// = [0,1,2,3,4,5] 
+// i = 0
+// storage[0] = storage[1] -> [1,1,2,3,4,5]
+// i = 1
+// storage[1] = storage[2] -> [1,2,2,3,4,5]
+// 
+// someInstance
+// stack1.storage ={}
+//stack1.push('a') -> {'a'}
+// stack2.storage = {1,2,3,4}
+// stack2.push('a')- >[1,2,3,4,'a']
+// 
 
   someInstance.enqueue = function(value){
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function(){
+    var temp = storage[0];
+    for(var i = 0; i < someInstance.size(); i++) {
+      storage[i] = storage[i + 1];
+    }
+    delete storage[someInstance.size() - 1];
+    return temp;
   };
 
   someInstance.size = function(){
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,23 @@ var Stack = function(){
 
   // Implement the methods below
   someInstance.push = function(value){
+    //add an element to the top of the stack i.e. Stack.push(element);
+    //storage[newIndex] = value;
+    //storage = {0:a,1:b}
+    //storage[2] = c
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function(){
+    //removes the very top element from the stack
+    var temp = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return temp;
   };
 
   someInstance.size = function(){
+    //returns how many elements are in storage
+    return Object.keys(storage).length;
   };
 
   return someInstance;
