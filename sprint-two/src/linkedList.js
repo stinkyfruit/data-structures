189547df40c75ad21ab6731ef5747abf809b0eef
Kristen Haydel <kristen@whatever.com>
2015-11-20 14:44:34 -0600

    Reimplemented Set and LinkedList Sprint 2

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,42 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    //create new node that will be the new tail
+    //create node
+    var newNode = Node(value);
+    if(list.head === null) {
+      list.head = newNode;
+    }
+    //set current tail's next to this node
+    if(list.tail !== null) {
+      list.tail.next = newNode;
+    }
+    
+    //next is going to be null
+    //set list.tail to new node
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var oldHead = list.head;
+    list.head = list.head.next;
+    return oldHead.value;
   };
 
-  list.contains = function(target){
+  list.contains = function(target, node){
+    node = node || this.head;
+    if (node.value === target) {
+      return true;
+    } else if (node.next !== null) {
+      return this.contains(target, node.next);
+    } else {
+      return false;
+    }
+    //if currentnode.value is target
+      //return true
+    //otherwise
+      //if next exists, recurse
+      //otherwise, return false
   };
 
   return list;
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,32 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
+  set.currentIndex = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  //O(n)
+  if(!this.contains(item)) {
+    this._storage[this.currentIndex] = item;
+    this.currentIndex++;
+  }
 };
 
 setPrototype.contains = function(item){
+  //O(n)
+  return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item){
+  //O(n)
+  for(var prop in this._storage) {
+    if(item === this._storage[prop]) {
+      delete this._storage[prop];
+    }
+  }
 };
 
 /*
